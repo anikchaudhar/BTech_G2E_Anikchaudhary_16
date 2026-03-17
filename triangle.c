4c40fb595bd778fa467f7feebb4eb0df9d30da9a
AnikChaudhary07 <107002125+anikchaudhar@users.noreply.github.com>
2026-03-17 15:41:01 +0530

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int a,c,d;
+    printf("enter sides of triangle");
+    scanf("%d%d%d",&a,&c,&d);
+    if ((a+c)>d || (c+d)>a || (a+d)>c){
+        printf("Yes it can form a triangle\n");
+    }
+    else{
+        printf("It will not form a triangle");
+    }
+    if (a==c && c == d){
+        printf("it is a equilateral triangle");
+    }
+    else if (a!=c && c!=d){
+        printf("it is a scalar triangle");
+    }
+    else {
+        printf("it is a isosceles");
+    }
+    return 0;
+}
